79659e591fc62a3408e148126c7fd5b2eab781f0
BagasKuning <dwibagaskara55@gmail.com>
2024-08-11 00:20:49 +0700

    animations to get around lengthy renderings

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,15 @@
+import { useState } from 'react'
 import me from '../img/me2-edit.jpg'
 
 export default function Navbar() {
+    const [anim, setAnim] = useState(false)
+
+    const handleImageLoad = () => {
+        setTimeout(() => {
+            setAnim(true)
+        }, 500)
+    }
+
     return (
         <nav className=' md:text-base text-xs z-10 lg:w-[55%] md:w-[70%] w-[96%] my-10 min-w-max flex bg-black text-white items-center justify-around md:py-5 md:px-8 py-4 px-5 rounded-full'>
 
@@ -15,7 +24,10 @@ export default function Navbar() {
             <div className='w-16' />
             <img src={me}
                 alt=""
-                className=' md:w-24 md:h-24 w-16 h-16 rounded-full absolute border-black border-4'
+                onLoad={handleImageLoad}
+                className={`md:w-24 md:h-24 w-16 h-16 rounded-full absolute border-black border-4
+                        ${anim === true ? "animate-fall" : " -translate-y-[150px]"}
+                    `}
             />
 
             <a href="#project">
