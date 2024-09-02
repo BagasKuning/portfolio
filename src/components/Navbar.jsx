fe38e61a3826bd42699cc586d5df7e8a4f53cf42
BagasKuning <dwibagaskara55@gmail.com>
2024-09-02 22:43:34 +0700

    fix: about image animation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,12 @@ export default function Navbar() {
     }
 
     return (
-        <nav className=' md:text-base text-xs z-10 lg:w-[55%] md:w-[70%] w-[96%] my-10 min-w-max flex bg-black text-white items-center justify-around md:py-5 md:px-8 py-4 px-5 rounded-full'>
+        <nav 
+        onLoad={handleImageLoad}
+        className={`md:text-base text-xs z-10 lg:w-[55%] md:w-[70%] w-[96%] my-10 min-w-max 
+                    flex bg-black text-white items-center justify-around md:py-5 md:px-8 py-4 px-5 rounded-full
+                    ${anim === true ? "animate-fall" : " -translate-y-[150px]"}
+            `}>
 
             <span className='relative before:absolute before:w-full before:h-[2px] before:bottom-0 before:bg-secondary'>
                 Portfolio
@@ -24,10 +29,7 @@ export default function Navbar() {
             <div className='w-16' />
             <img src={me}
                 alt=""
-                onLoad={handleImageLoad}
-                className={`md:w-24 md:h-24 w-16 h-16 rounded-full absolute border-black border-4
-                        ${anim === true ? "animate-fall" : " -translate-y-[150px]"}
-                    `}
+                className={`md:w-24 md:h-24 w-16 h-16 rounded-full absolute border-black border-4`}
             />
 
             <a href="#project">
